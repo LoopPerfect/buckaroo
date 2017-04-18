a9edc3be1d009b4683c49a5fd774af56d933eec2
njlr <njlr@users.noreply.github.com>
2017-04-18 15:05:53 +0100

    * Added touch function to fs  * Quickstart now touches .buckconfig and BUCKAROO_DEPS

--- src/main/java/com/loopperfect/buckaroo/routines/Quickstart.java
+++ src/main/java/com/loopperfect/buckaroo/routines/Quickstart.java
@@ -34,16 +34,18 @@ public final class Quickstart {
     private static IO<Optional<IOException>> createAppSkeleton(final String path, final Identifier projectName) {
         Preconditions.checkNotNull(projectName);
         return Routines.continueUntilPresent(ImmutableList.of(
-                        IO.createDirectory(path + "/"),
-                        IO.createDirectory(path + "/" + projectName + "/"),
-                        IO.createDirectory(path + "/" + projectName + "/src/"),
-                        IO.createDirectory(path + "/" + projectName + "/include/"),
-                        helloWorldCpp().join(
-                                error -> IO.value(Optional.of(error)),
-                                content -> IO.writeFile(path + "/" + projectName + "/src/main.cpp", content, false)),
-                        BuckFile.generate(projectName).join(
-                                error -> IO.value(Optional.of(error)),
-                                buck -> IO.writeFile(path + "/BUCK", buck, false))));
+            IO.createDirectory(path + "/"),
+            IO.of(context -> context.fs().touch(path + "/" + ".buckconfig")),
+            IO.of(context -> context.fs().touch(path + "/" + "BUCKAROO_DEPS")),
+            IO.createDirectory(path + "/" + projectName + "/"),
+            IO.createDirectory(path + "/" + projectName + "/src/"),
+            IO.createDirectory(path + "/" + projectName + "/include/"),
+            helloWorldCpp().join(
+                error -> IO.value(Optional.of(error)),
+                content -> IO.writeFile(path + "/" + projectName + "/src/main.cpp", content, false)),
+            BuckFile.generate(projectName).join(
+                error -> IO.value(Optional.of(error)),
+                buck -> IO.writeFile(path + "/BUCK", buck, false))));
     }
 
     public static final IO<Unit> routine = IO.of(x -> x.fs().workingDirectory())
