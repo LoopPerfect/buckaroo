82b3a4cb49162accf43059bae6b5d3a3e65bbb70
njlr <njlr@users.noreply.github.com>
2017-07-06 16:31:04 +0100

    * Sort resolved dependencies by name

--- src/main/java/com/loopperfect/buckaroo/views/GenericEventRenderer.java
+++ src/main/java/com/loopperfect/buckaroo/views/GenericEventRenderer.java
@@ -16,6 +16,7 @@ import com.loopperfect.buckaroo.virtualterminal.TerminalPixel;
 import com.loopperfect.buckaroo.virtualterminal.UnicodeChar;
 import com.loopperfect.buckaroo.virtualterminal.components.*;
 
+import java.util.Comparator;
 import java.util.List;
 
 public final class GenericEventRenderer {
@@ -119,6 +120,7 @@ public final class GenericEventRenderer {
             ListLayout.of(
                 event.dependencies.dependencies.entrySet()
                     .stream()
+                    .sorted(Comparator.comparing(x -> x.getKey().encode()))
                     .map(x -> FlowLayout.of(
                         render(x.getKey()),
                         Text.of(" = "),
