166282f04d2b13c5de38ae95d7804e780a886d79
njlr <njlr@users.noreply.github.com>
2017-07-10 11:04:05 +0100

    * Default cookbook now uses HTTPS URL  * Bumped the version

--- src/main/java/com/loopperfect/buckaroo/Buckaroo.java
+++ src/main/java/com/loopperfect/buckaroo/Buckaroo.java
@@ -6,5 +6,5 @@ public final class Buckaroo {
 
     }
 
-    public static final SemanticVersion version = SemanticVersion.of(0, 1, 1);
+    public static final SemanticVersion version = SemanticVersion.of(0, 1, 2);
 }
