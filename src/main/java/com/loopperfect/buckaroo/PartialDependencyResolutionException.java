256e0c95e52945f176fe6ed2ac334cd6b3d2d96e
njlr <njlr@users.noreply.github.com>
2017-07-12 18:44:21 +0100

    * Improved the reporting of partial dependency errors

--- src/main/java/com/loopperfect/buckaroo/PartialDependencyResolutionException.java
+++ src/main/java/com/loopperfect/buckaroo/PartialDependencyResolutionException.java
@@ -7,20 +7,18 @@ import com.loopperfect.buckaroo.virtualterminal.components.*;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 
-/**
- * Created by gaetano on 12/07/17.
- */
-public class PartialDependencyResolutionException
-    extends Exception
-    implements RenderableException {
+public class PartialDependencyResolutionException extends Exception implements RenderableException {
+
     final ImmutableList<RecipeIdentifier> candidates;
     final PartialDependency requested;
 
     private PartialDependencyResolutionException(
         final ImmutableList<RecipeIdentifier> candidates,
         final PartialDependency requested) {
+
         this.candidates = Preconditions.checkNotNull(candidates);
         this.requested = Preconditions.checkNotNull(requested);
+        Preconditions.checkArgument(candidates.size() != 1);
     }
 
     public static PartialDependencyResolutionException of(
@@ -34,15 +32,16 @@ public class PartialDependencyResolutionException
 
     @Override
     public Component render() {
+
         if (candidates.size() == 0) {
             return StackLayout.of(
-                Text.of("PartialDependencyCompletionError!", Color.RED),
+                Text.of("Partial dependencies could not be resolved!", Color.RED),
                 FlowLayout.of(
                     Text.of("Could not find what you are looking for... you requested: "),
                     Text.of(requested.toString(), Color.YELLOW)),
-                Text.of("This might help:"),
+                Text.of("This might help: "),
                 ListLayout.of(
-                    Text.of("Ensure you don't have a typo"),
+                    Text.of("Ensure that you don't have a typo"),
                     FlowLayout.of(
                         Text.of("Specify an organization eg.: "), Text.of("boost/",Color.MAGENTA), Text.of("asio")),
                     FlowLayout.of(
@@ -57,15 +56,15 @@ public class PartialDependencyResolutionException
 
         if (candidates.size() > 1) {
             return StackLayout.of(
+                Text.of("Partial dependencies could not be resolved!", Color.RED),
                 FlowLayout.of(
-                    Text.of("PartialDependencyCompletionError!", Color.RED),
                     Text.of("Your request for "),
                     Text.of(requested.toString(), Color.BLUE),
-                    Text.of(" is ambiguous")),
-                Text.of("do you want to install one of these packages?"),
+                    Text.of(" is ambiguous. ")),
+                Text.of("Maybe you wanted to install one of these packages?"),
                 ListLayout.of(
                     candidates.stream().map(
-                        c-> FlowLayout.of(
+                        c -> FlowLayout.of(
                                 Text.of(c.organization.toString(), Color.MAGENTA),
                                 Text.of("/"),
                                 Text.of(c.recipe.toString())
@@ -73,12 +72,10 @@ public class PartialDependencyResolutionException
         }
 
         return StackLayout.of(
-            Text.of("PartialDependencyCompletionError!", Color.RED),
-            Text.of("Sry but something went wrong resolving: " + candidates.get(0).toString(), Color.RED),
+            Text.of("Partial dependencies could not be resolved!", Color.RED),
+            Text.of("Something went wrong resolving: " + candidates.get(0).toString(), Color.RED),
             FlowLayout.of(
-                Text.of("please report to "),
-                Text.of("https://github.com/loopperfect/buckaroo/issues", Color.BLUE)
-            )
-        );
+                Text.of("Please report this issue to "),
+                Text.of("https://github.com/loopperfect/buckaroo/issues", Color.BLUE)));
     }
 }
