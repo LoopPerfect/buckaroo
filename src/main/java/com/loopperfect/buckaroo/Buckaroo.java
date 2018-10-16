3a24d7be91b3783532d77210c0583d1460f22d5f
njlr <njlr@users.noreply.github.com>
2018-10-16 17:43:25 +0100

    * Bumps version

--- src/main/java/com/loopperfect/buckaroo/Buckaroo.java
+++ src/main/java/com/loopperfect/buckaroo/Buckaroo.java
@@ -6,5 +6,5 @@ public final class Buckaroo {
 
     }
 
-    public static final SemanticVersion version = SemanticVersion.of(1, 4, 0);
+    public static final SemanticVersion version = SemanticVersion.of(1, 4, 1);
 }
