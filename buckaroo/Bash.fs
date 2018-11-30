88ef19faaa5b05220fb76aff8d8deaadf937ba3e
njlr <njlr@users.noreply.github.com>
2018-11-30 11:49:41 +0000

    * Adds async bash implementation

--- buckaroo/Bash.fs
+++ buckaroo/Bash.fs
@@ -78,3 +78,60 @@ let runBash (command : string) = asyncSeq {
 
   yield Exit p.ExitCode
 }
+
+type ProgressCallback = string -> Unit
+
+let escapeBash (command : string) = 
+  if command.Contains("\"") || command.Contains("$") 
+  then 
+    raise <| new Exception("Malicious bash? " + command)
+  command
+
+let runBashAsync (command : string) (stdoutHandler : ProgressCallback) (stderrHandler : ProgressCallback) = async {
+  let startInfo = new ProcessStartInfo()
+
+  startInfo.CreateNoWindow <- true
+  startInfo.UseShellExecute <- false
+  startInfo.FileName <- "/bin/bash"
+  startInfo.Arguments <- "-c \"" + (escapeBash command) + "\""
+  startInfo.RedirectStandardOutput <- true
+  startInfo.RedirectStandardError <- true
+  startInfo.RedirectStandardInput <- true
+  startInfo.WindowStyle <- ProcessWindowStyle.Hidden
+
+  let p = new Process()
+
+  p.StartInfo <- startInfo
+  p.EnableRaisingEvents <- true
+
+  let! exitSignal = 
+    p.Exited
+    |> Async.AwaitEvent
+    |> Async.Ignore 
+    |> Async.StartChild
+
+  p.OutputDataReceived.AddHandler(new DataReceivedEventHandler(fun _ event -> 
+    if event.Data <> null
+    then
+      stdoutHandler event.Data
+  ))
+
+  p.ErrorDataReceived.AddHandler(new DataReceivedEventHandler(fun _ event -> 
+    if event.Data <> null
+    then
+      stderrHandler event.Data
+  ))
+
+  p.Start() |> ignore
+
+  use! cancelHandler = 
+    Async.OnCancel (fun () -> p.Kill())
+
+  p.BeginOutputReadLine()
+  p.BeginErrorReadLine()
+
+  do! exitSignal
+
+  return p.ExitCode
+}
+
