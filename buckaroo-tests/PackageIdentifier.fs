0c7336ec3f624c9b761dce2029abdabbceed1daa
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-12-07 14:44:53 +0000

    feat: adds parser for git

--- buckaroo-tests/PackageIdentifier.fs
+++ buckaroo-tests/PackageIdentifier.fs
@@ -15,8 +15,16 @@ let ``PackageIdentifier.parse works correctly`` () =
   let cases = [
     ("github.com/abc/def", PackageIdentifier.GitHub { Owner = "abc"; Project = "def" } |> Some);
     ("github.com/abc/def.ghi", PackageIdentifier.GitHub { Owner = "abc"; Project = "def.ghi" } |> Some);
-    ("github+abc/def", PackageIdentifier.GitHub { Owner = "abc"; Project = "def" } |> Some);
-    ("github+abc/def_ghi", PackageIdentifier.GitHub { Owner = "abc"; Project = "def_ghi" } |> Some);
+    ("gh+abc/def", PackageIdentifier.GitHub { Owner = "abc"; Project = "def" } |> Some);
+    ("gh+abc/def_ghi", PackageIdentifier.GitHub { Owner = "abc"; Project = "def_ghi" } |> Some);
+    ("git://user@test.com:1337/path/to.git", 
+      PackageIdentifier.Git { Protocol = "git"; Uri = "user@test.com:1337/path/to.git" } |> Some);
+    ("git+ssh://user@test.com:1337/path/to.git", 
+      PackageIdentifier.Git { Protocol = "ssh"; Uri = "user@test.com:1337/path/to.git" } |> Some);
+    ("git+file://./relative/path", 
+      PackageIdentifier.Git { Protocol = "file"; Uri = "./relative/path" } |> Some);
+    ("git+http://user:password@test.com/user/project.git", 
+      PackageIdentifier.Git { Protocol = "http"; Uri = "user:password@test.com/user/project.git" } |> Some);
     ("", None)
   ]
   for (input, expected) in cases do
--- buckaroo/PackageIdentifier.fs
+++ buckaroo/PackageIdentifier.fs
@@ -1,11 +1,12 @@
 namespace Buckaroo
 
 type GitHubPackageIdentifier = { Owner : string; Project : string }
-
+type GitPackageIdentifier = { Protocol : string; Uri : string }
 type AdhocPackageIdentifier = { Owner : string; Project : string }
 
 type PackageIdentifier = 
 | GitHub of GitHubPackageIdentifier
+| Git of GitPackageIdentifier
 | Adhoc of AdhocPackageIdentifier
 
 module PackageIdentifier = 
@@ -15,10 +16,22 @@ module PackageIdentifier =
   let show (id : PackageIdentifier) = 
     match id with
     | GitHub x -> "github.com/" + x.Owner + "/" + x.Project
+    | Git x -> x.Protocol + x.Uri
     | Adhoc x -> x.Owner + "/" + x.Project
 
   let private gitHubIdentifierParser = 
     CharParsers.regex @"[a-zA-Z.\d](?:[a-zA-Z_.\d]|-(?=[a-zA-Z_.\d])){0,38}"
+
+  let private gitUriParser = 
+    CharParsers.regex @"[a-zA-Z.\d:@/-~]+"
+
+  let private gitProtocolParser = parse {
+    let! protocol = CharParsers.regex @"(ssh|file|rsync|http|https)"
+    do! CharParsers.skipString "://"
+    return protocol
+  }
+   
+ 
 
   let adhocPackageIdentifierParser = parse {
     let! owner = gitHubIdentifierParser
@@ -34,11 +47,24 @@ module PackageIdentifier =
 
 
   let gitHubPackageIdentifierParser = parse {
-    do! CharParsers.skipString "github.com/" <|> CharParsers.skipString "github+"
+    do! CharParsers.skipString "github.com/" 
+      <|> CharParsers.skipString "gh+"
     let! owner = gitHubIdentifierParser
     do! CharParsers.skipString "/"
     let! project = gitHubIdentifierParser
     return ({ Owner = owner; Project = project } : GitHubPackageIdentifier)
+  }
+
+  let gitPackageIdentifierParser = parse {
+    let! prefix =  CharParsers.pstring "git+" <|> CharParsers.pstring "git://"
+    let! protocol =
+      match prefix with
+      | "git://" -> parse { return "git" }
+      | "git+" -> gitProtocolParser
+      | _ -> parse { return "git" } // should never happen
+
+    let! uri = gitUriParser
+    return ({ Protocol = protocol; Uri = uri } : GitPackageIdentifier)
   }
 
   let parseGitHubIdentifier (x : string) = 
@@ -48,6 +74,7 @@ module PackageIdentifier =
 
   let parser =
     gitHubPackageIdentifierParser |>> PackageIdentifier.GitHub
+    <|> (gitPackageIdentifierParser |>> PackageIdentifier.Git)
     <|> (adhocPackageIdentifierParser |>> PackageIdentifier.Adhoc)
 
   let parse (x : string) : Result<PackageIdentifier, string> = 
