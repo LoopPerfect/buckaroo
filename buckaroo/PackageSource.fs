360dcbd6f144651ba27272718ed80ed81c134e44
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-12-11 15:44:05 +0000

    feat: better location conflict error messages

--- buckaroo/PackageSource.fs
+++ buckaroo/PackageSource.fs
@@ -17,14 +17,20 @@ type PackageSource =
 
 module PackageSource =
   let show = function
-  | Git g -> "git = " + g.Uri
+  | Git g -> "{ git = " + g.Uri + " }"
   | Http h ->
     "{\n" + (
       h
       |> Map.toSeq
       |> Seq.map(fun (version, source) ->
-          "Version = \"" + version.ToString()  + "\"" +
-          "Url = \"" + source.Url + "\"\n" + "}"
+          "Version = \"" + version.ToString()  + "\"\n" +
+          "Url = \"" + source.Url + "\"\n" +
+          (source.StripPrefix
+            |> Option.map(fun x -> "StripPrefix =\"" + x + "\"\n")
+            |> Option.defaultValue("") ) +
+          (source.Type
+            |> Option.map(fun x -> "Type =\"" + x.ToString() + "\"\n")
+            |> Option.defaultValue("") ) + "\n"
         )
       |> String.concat "\n") +
     "\n}"
