6ed83f0191ff70f08ee3ba684aa5c574f3f90672
njlr <njlr@users.noreply.github.com>
2018-12-20 14:40:59 +0000

    * Further test for Target parser

--- buckaroo-tests/Target.fs
+++ buckaroo-tests/Target.fs
@@ -6,27 +6,31 @@ open Xunit
 open Buckaroo
 
 [<Fact>]
-let ``Target.parse works correctly`` () = 
+let ``Target.parse works correctly`` () =
   let input = "//path/to/some:target"
   let expected = Result.Ok { Folders = [ "path"; "to"; "some" ]; Name = "target" }
   Assert.Equal(expected, Target.parse input)
-  
+
   let input = "//:foo"
   let expected = Result.Ok { Folders = []; Name = "foo" }
   Assert.Equal(expected, Target.parse input)
-  
+
   let input = "//foo"
   let expected = Result.Ok { Folders = [ "foo" ]; Name = "foo" }
   Assert.Equal(expected, Target.parse input)
-  
+
   let input = "//foo/bar"
   let expected = Result.Ok { Folders = [ "foo"; "bar" ]; Name = "bar" }
   Assert.Equal(expected, Target.parse input)
-  
+
   let input = "//abc/def"
   let expected = Result.Ok { Folders = [ "abc"; "def" ]; Name = "def" }
   Assert.Equal(expected, Target.parse input)
 
   let input = ":bar"
   let expected = Result.Ok { Folders = []; Name = "bar" }
+  Assert.Equal(expected, Target.parse input)
+
+  let input = "foo:bar"
+  let expected = Result.Ok { Folders = [ "foo" ]; Name = "bar" }
   Assert.Equal(expected, Target.parse input)
