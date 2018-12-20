ee170814959815ded1b9db829e31299b6a456d80
njlr <njlr@users.noreply.github.com>
2018-12-20 23:09:06 +0000

    * Adds further test of complement

--- buckaroo-tests/Constraint.fs
+++ buckaroo-tests/Constraint.fs
@@ -6,20 +6,34 @@ open Xunit
 open Buckaroo
 
 let dropError<'T, 'E> (x : Result<'T, 'E>) =
-  match x with 
+  match x with
   | Result.Ok o -> Some o
   | Result.Error _ -> None
 
 [<Fact>]
 let ``Constraint.parse works correctly`` () =
   let cases = [
-    ("*", Constraint.wildcard |> Some); 
-    ("revision=aabbccddee", "aabbccddee" |> Version.Revision |> Exactly |> Some); 
-    ("!*", Constraint.wildcard |> Constraint.Complement |> Some); 
-    ("any(branch=master)", Some(Any [Exactly (Version.Branch "master")])); 
-    ("any(revision=aabbccddee branch=master)", Some(Any [Exactly (Version.Revision "aabbccddee"); Exactly (Version.Branch "master")])); 
-    ("all(*)", Some(All [Constraint.wildcard])); 
-    ("", None); 
+    ("*", Constraint.wildcard |> Some);
+    ("revision=aabbccddee", "aabbccddee" |> Version.Revision |> Exactly |> Some);
+    ("!*", Constraint.wildcard |> Constraint.Complement |> Some);
+    ("any(branch=master)", Some (Any [Exactly (Version.Branch "master")]));
+    ("any(revision=aabbccddee branch=master)", Some (Any [Exactly (Version.Revision "aabbccddee"); Exactly (Version.Branch "master")]));
+    ("all(*)", Some (All [Constraint.wildcard]));
+    (
+      "all(branch=master !revision=aabbccddee)",
+      Some (All [Exactly (Version.Branch "master"); Complement (Exactly (Version.Revision "aabbccddee"))])
+    );
+    (
+      "all(branch=master !any(revision=aabbccddee branch=develop))",
+      Some (All [
+        Exactly (Version.Branch "master");
+        Complement (Any([
+          Exactly (Version.Revision "aabbccddee");
+          Exactly (Version.Branch "develop");
+        ]))
+      ])
+    );
+    ("", None);
   ]
   for (input, expected) in cases do
     Assert.Equal(expected, Constraint.parse input |> dropError)
@@ -44,17 +58,17 @@ let ``Constraint.agreesWith works correctly`` () =
 
 [<Fact>]
 let ``Constraint.compare works correctly`` () =
-  let input = [ 
-    (Constraint.Exactly <| Version.Branch "master"); 
-    (Constraint.Exactly <| Version.Tag "v1.0.0"); 
-    (Constraint.wildcard); 
-    (Constraint.Exactly <| Version.Revision "aabbccddee"); 
+  let input = [
+    (Constraint.Exactly <| Version.Branch "master");
+    (Constraint.Exactly <| Version.Tag "v1.0.0");
+    (Constraint.wildcard);
+    (Constraint.Exactly <| Version.Revision "aabbccddee");
   ]
-  let expected = [ 
-    (Constraint.Exactly <| Version.Revision "aabbccddee"); 
-    (Constraint.Exactly <| Version.Tag "v1.0.0"); 
-    (Constraint.Exactly <| Version.Branch "master"); 
-    (Constraint.wildcard); 
+  let expected = [
+    (Constraint.Exactly <| Version.Revision "aabbccddee");
+    (Constraint.Exactly <| Version.Tag "v1.0.0");
+    (Constraint.Exactly <| Version.Branch "master");
+    (Constraint.wildcard);
   ]
   let actual = input |> List.sortWith Constraint.compare
   Assert.Equal<List<Constraint>>(expected, actual)
