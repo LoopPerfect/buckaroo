656d75f7d8630aedaf87236fe7efdb01840c5026
njlr <njlr@users.noreply.github.com>
2019-01-08 14:02:46 +0000

    * Adds Warp to Windows bundle script

--- warp-bundle-windows.ps1
+++ warp-bundle-windows.ps1
@@ -1,1 +1,9 @@
+wget https://github.com/dgiagio/warp/releases/download/v0.3.0/windows-x64.warp-packer.exe -OutFile warp-packer.exe
+
 dotnet publish ./buckaroo-cli/ -c Release -r win10-x64
+
+md .\warp -ea 0
+
+.\warp-packer.exe --arch windows-x64 --input_dir bin/Release/netcoreapp2.1/win10-x64/publish --exec ./warp/buckaroo-windows.exe --output ./warp/buckaroo-windows.exe
+
+.\warp\buckaroo-windows.exe
