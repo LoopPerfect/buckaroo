ddcdb7417edce943296147d56485ce17b391af53
njlr <njlr@users.noreply.github.com>
2019-01-08 14:07:47 +0000

    * Fixes input dir

--- warp-bundle-windows.ps1
+++ warp-bundle-windows.ps1
@@ -4,6 +4,6 @@ dotnet publish ./buckaroo-cli/ -c Release -r win10-x64
 
 md .\warp -ea 0
 
-.\warp-packer.exe --arch windows-x64 --input_dir bin/Release/netcoreapp2.1/win10-x64/publish --exec ./warp/buckaroo-windows.exe --output ./warp/buckaroo-windows.exe
+.\warp-packer.exe --arch windows-x64 --input_dir ./buckaroo-cli/bin/Release/netcoreapp2.1/win10-x64 --exec ./warp/buckaroo-windows.exe --output ./warp/buckaroo-windows.exe
 
 .\warp\buckaroo-windows.exe
