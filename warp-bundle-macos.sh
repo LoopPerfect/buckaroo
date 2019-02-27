f2acd1998ba3ba81a2bc304c2812fd60a28b4dde
njlr <njlr@users.noreply.github.com>
2019-02-27 13:47:54 +0000

    * Reduces the size of the final bundle

--- warp-bundle-macos.sh
+++ warp-bundle-macos.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-wget -O warp-packer https://github.com/dgiagio/warp/releases/download/v0.3.0/macos-x64.warp-packer 
+wget -O warp-packer https://github.com/dgiagio/warp/releases/download/v0.3.0/macos-x64.warp-packer
 
 chmod +x ./warp-packer
 ./warp-packer --version
 
-dotnet publish ./buckaroo-cli/ -c Release -r osx-x64 
+dotnet publish ./buckaroo-cli/ -c Release -r osx-x64
 
 mkdir -p warp
+rm -rf ./warp/buckaroo-macos
 
 ./warp-packer --arch macos-x64 --exec buckaroo-cli --input_dir ./buckaroo-cli/bin/Release/netcoreapp2.1/osx-x64   --output warp/buckaroo-macos
 ./warp/buckaroo-macos
